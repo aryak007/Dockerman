e9f9ab8dbef570c39009b2989d27b03654b248a7
Aryak Sengupta <aryak.sengupta@lexmark.com>
2018-08-05 13:21:49 +0530

    Mocked data and dummy REST endpoints

--- controllers/Images-controller.js
+++ controllers/Images-controller.js
@@ -1,74 +1,83 @@
 'use strict';
 var Commons = require('../util/Commons.js')
+const fs = require('fs');
 const { exec } = require('child_process');
 
 
 function listImagesInfo(req, res) {
-    exec('curl --unix-socket /var/run/docker.sock http:/v1.24/images/json', (err, stdout, stderr) => {
-      if (err) {
-        console.log(err+" "+`${stdout}`);
-        res.send(`stderr`+' '+err);
-        return;
-      }
-      var imageInfo = [];
-      var imagesRawData = []
-      imagesRawData = JSON.parse(`${stdout}`)
-      imagesRawData.forEach(function(imageData) {
+    // exec('curl --unix-socket /var/run/docker.sock http:/v1.24/images/json', (err, stdout, stderr) => {
+    //   if (err) {
+    //     console.log(err+" "+`${stdout}`);
+    //     res.send(`stderr`+' '+err);
+    //     return;
+    //   }
+    //   var imageInfo = [];
+    //   var imagesRawData = []
+    //   imagesRawData = JSON.parse(`${stdout}`)
+    //   imagesRawData.forEach(function(imageData) {
 
 
-            var imageRepoTags = imageData.RepoTags;
-            //console.log(imageRepoTags+" image repo tags");
-            if(imageRepoTags!=null && !Commons.isNone(imageRepoTags[0]))
-            {
-                var displayName = Commons.returnImageNameFromRepoTag(imageRepoTags[0]);
-                var fullName = imageRepoTags[0]
-                var commitId = imageData.Id
-                var size = Commons.convertBytesToHumanReadableFormat(imageData.Size);
-                var runningContainers = imageData.Containers;
-                //var date = Commons.getFormattedCreatedDateFromTimeStamp(imageData.Created);
+    //         var imageRepoTags = imageData.RepoTags;
+    //         //console.log(imageRepoTags+" image repo tags");
+    //         if(imageRepoTags!=null && !Commons.isNone(imageRepoTags[0]))
+    //         {
+    //             var displayName = Commons.returnImageNameFromRepoTag(imageRepoTags[0]);
+    //             var fullName = imageRepoTags[0]
+    //             var commitId = imageData.Id
+    //             var size = Commons.convertBytesToHumanReadableFormat(imageData.Size);
+    //             var runningContainers = imageData.Containers;
+    //             //var date = Commons.getFormattedCreatedDateFromTimeStamp(imageData.Created);
 
-                //console.log(imageRepoTags)
-                imageInfo.push({
-                    imageRepoTags:imageRepoTags,
-                    commitId:commitId,
-                    displayName:displayName,
-                    fullName:fullName,
-                    size:size,
-                    runningContainers:runningContainers
-                })
-            }
-        })
-      res.json(imageInfo)
-    });
+    //             //console.log(imageRepoTags)
+    //             imageInfo.push({
+    //                 imageRepoTags:imageRepoTags,
+    //                 commitId:commitId,
+    //                 displayName:displayName,
+    //                 fullName:fullName,
+    //                 size:size,
+    //                 runningContainers:runningContainers
+    //             })
+    //         }
+    //     })
+    //   res.json(imageInfo)
+    // });
+    fs.readFile("./data/dummy.json",function(err,imageInfo){
+        if(err)
+          throw err;
+        res.json(JSON.parse(imageInfo))
+    })
 
 }
 
 function removeDockerImage(req,res){
-    var commitId = req.params.commitId
-    console.log("-------------Removing image with "+commitId+" ------------")
-    exec('docker rmi '+commitId+' -f', (err, stdout, stderr) => {
-        if (err) {
-        console.log(err);
-        res.send(`stderr`+' '+err);
-        return;
-      }
-      console.log(commitId)
-      res.json(commitId+" deleted")
-    });
+   var commitId = req.params.commitId
+    // console.log("-------------Removing image with "+commitId+" ------------")
+    // exec('docker rmi '+commitId+' -f', (err, stdout, stderr) => {
+    //     if (err) {
+    //     console.log(err);
+    //     res.send(`stderr`+' '+err);
+    //     return;
+    //   }
+    //   console.log(commitId)
+    //   res.json(commitId+" deleted")
+    // });
+    res.json(commitId+" dummy commit id deleted")
 }
 
 function removeImageTag(req,res){
-    var tagName = req.params.tagName
-    console.log("-------------Removing "+tagName+" ------------")
-    exec('docker rmi '+tagName, (err, stdout, stderr) => {
-        if (err) {
-        console.log(err);
-        res.send(`stderr`+' '+err);
-        return;
-      }
-      //console.log(commitId)
-      res.json(tagName+" deleted")
-    });
+     var tagName = req.params.tagName
+    // console.log("-------------Removing "+tagName+" ------------")
+    // exec('docker rmi '+tagName, (err, stdout, stderr) => {
+    //     if (err) {
+    //     console.log(err);
+    //     res.send(`stderr`+' '+err);
+    //     return;
+    //   }
+    //   //console.log(commitId)
+    //   res.json(tagName+" deleted")
+    // });
+    res.json(tagName+" dummy tagName deleted")
+
 }
 
 function newTagForImage(req,res)
@@ -79,30 +88,32 @@ function newTagForImage(req,res)
     console.log("ImageName "+imageName)
 
     console.log("-------------Tagging image "+imageName+" with "+tagName+" ------------")
-    exec('docker tag '+imageName+' '+tagName, (err, stdout, stderr) => {
-        if (err) {
-        console.log(err);
-        res.send(`stderr`+' '+err);
-        return;
-      }
-      //console.log(commitId)
-      res.json(imageName+" tagged with "+tagName)
-    });
+    // exec('docker tag '+imageName+' '+tagName, (err, stdout, stderr) => {
+    //     if (err) {
+    //     console.log(err);
+    //     res.send(`stderr`+' '+err);
+    //     return;
+    //   }
+    //   //console.log(commitId)
+    //   res.json(imageName+" tagged with "+tagName)
+    // });
+    res.json(imageName+" tagged with "+tagName)
 }
 
 function pushImage(req,res)
 {
     var imageName = req.params.imageName
     
-    exec('docker push '+imageName, (err, stdout, stderr) => {
-        if (err) {
-        console.log(err);
-        res.json(`stderr`+' '+err);
-        return;
-      }
-      console.log(commitId)
-      res.json(imageName+ " pushed to repository")
-    });
+    // exec('docker push '+imageName, (err, stdout, stderr) => {
+    //     if (err) {
+    //     console.log(err);
+    //     res.json(`stderr`+' '+err);
+    //     return;
+    //   }
+    //   console.log(commitId)
+    //   res.json(imageName+ " pushed to repository")
+    // });
+    res.json(imageName+ " pushed to repository")
 }
 
 
